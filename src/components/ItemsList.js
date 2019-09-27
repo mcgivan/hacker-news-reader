6f239eb800f1522aafe4df8896e10909e53bba0f
Taras Turchyn <mcgivan@gmail.com>
2019-09-27 23:44:03 +0200

    prepare for build

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Loader from "./Loader/Loader";
 
 const ItemsList = ViewComponent => itemIds => {
   return itemIds && itemIds.length ? (
     itemIds.map(id => <ViewComponent key={id} id={id} />)
   ) : (
-    <Loader />
+    null
   );
 };
 
